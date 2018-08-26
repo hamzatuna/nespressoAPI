1dc5080aedee1a0ae66fdc21a38bf0214f7666e4
ermissa <ermissa@itu.edu.tr>
2018-08-26 18:24:42 +0300

    Dashboard Templateleri MVC çalışacak şekilde eklendi , Satış bilgileri sekmesine datatables entegre edildi , Template inheritance henüz implemente edilmedi.

--- nespressoAPI/urls.py
+++ nespressoAPI/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path,include
 from rest_framework import generics
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -13,8 +14,9 @@ urlpatterns = [
     #path('insert_machines', views.machines, name="insert_machines"),
     path('auth', include('rest_framework.urls', namespace='rest_framework')),
     path('get-token', obtain_auth_token),
-
     path('home',views.home,name="home"),
-    #path('admin_get_sales',views.SalesViewSet.as_view({'get': 'list'}),name="admin_get_sales")
+    path('dashboard',TemplateView.as_view(template_name='dashboard_main.html'),name="dashboard_main"),
+    path('dashboard_sales_tab',TemplateView.as_view(template_name='dashboard_sales_tab.html'),name="dashboard_sales_tab"),
+    path('dashboard_tasting_tab',TemplateView.as_view(template_name='dashboard_tasting_tab.html'),name="dashboard_tasting_tab")
 ]
 
