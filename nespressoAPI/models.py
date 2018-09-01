2d85f2fdd8e95d612892ded6c5d264e48b364cb6
hamzatuna <tuanaham@itu.edu.tr>
2018-09-01 18:32:31 +0300

    some refactoring

--- nespressoAPI/models.py
+++ nespressoAPI/models.py
@@ -18,18 +18,25 @@ class Locations(models.Model):
     Latitude = models.FloatField()
     Longtitude = models.FloatField()
     LocationName = models.CharField(max_length=1000)
+    
     class Meta:
         db_table = "Locations"
 
 class Personnels(models.Model):
-    LocationId = models.ForeignKey(Locations,on_delete=models.CASCADE,db_column='LocationId')
     Name = models.CharField(max_length=200)
     Surname = models.CharField(max_length=200)
     Email = models.EmailField(max_length=200)
     Birthday = models.DateField()
     PhoneNumber = models.CharField(max_length=30)
     Wage = models.DecimalField(max_digits=10,decimal_places=6)
-    IsActive = models.BooleanField()    
+    IsActive = models.BooleanField()
+
+    # foreign keys
+    LocationId = models.ForeignKey(
+        Locations, 
+        on_delete=models.CASCADE,
+        db_column='LocationId')
+
     class Meta:
         db_table = "Personnels"
 
@@ -37,7 +44,11 @@ class Personnels(models.Model):
 class Machines(models.Model):
     Name = models.CharField(max_length=200)
     SerialNumber = models.CharField(max_length=1000)
-    Fee = models.DecimalField(max_digits=10,decimal_places=3) # max_digits noktadan sonrayla beraber toplam kaç sayı olabilir, decimal_places ise noktadan önceki ondalık kısımda max kaç sayı olabilir.
+    
+    # max_digits noktadan sonrayla beraber toplam kaç sayı olabilir, 
+    # decimal_places ise noktadan önceki ondalık kısımda max kaç sayı olabilir.
+    Fee = models.DecimalField(max_digits=10,decimal_places=3)
+    
     class Meta:
         db_table = "Machines"
 
@@ -46,45 +57,72 @@ class Supervisors(models.Model):
     Surname = models.CharField(max_length=200)
     Email = models.EmailField(max_length=200)
     PhoneNumber = models.CharField(max_length=30)
-    Location = models.ForeignKey(Locations,on_delete=models.CASCADE)
     IsActive = models.BooleanField()
+    
+    # foreign keys
+    Location = models.ForeignKey(Locations,on_delete=models.CASCADE)
+    
     class Meta:
         db_table = "Supervisors"
     
 class Sales(models.Model):
-    MachineId = models.ForeignKey(Machines,on_delete=models.CASCADE,db_column='MachineId')
-    PersonnelId = models.ForeignKey(Personnels,on_delete=models.CASCADE,db_column='PersonnelId')
-    #SerialNumber = models.CharField(max_length=1000) #Seri numarası tekrar eklemek gerekli mi düşün.
+   # SerialNumber = models.CharField(max_length=1000) 
+   # Seri numarası tekrar eklemek gerekli mi düşün.
     CustomerName = models.CharField(max_length=200)
     CustomerSurname = models.CharField(max_length=200)
     CustomerPhoneNumber = models.CharField(max_length=30)
     CustomerEmail = models.EmailField(max_length=200)
     IsCampaign = models.BooleanField()
+
+    # foreign keys
+    MachineId = models.ForeignKey(
+        Machines,
+        on_delete=models.CASCADE,
+        db_column='MachineId')
+    PersonnelId = models.ForeignKey(
+        Personnels,
+        on_delete=models.CASCADE,
+        db_column='PersonnelId')
+ 
     class Meta:
         db_table = "Sales"
 
 #Yoğun saatler tablosu
 class IntensiveHours(models.Model):
-    IntensiveHour = models.CharField(max_length=100) #Veritabanında 12.00-14.00 şeklinde kayıtlı olacak.
+    #Veritabanında 12.00-14.00 şeklinde kayıtlı olacak.
+    IntensiveHour = models.CharField(max_length=100)
+    
     class Meta:
         db_table = "IntensiveHours"
 
 class MachineConditions(models.Model):
     MachineCondition = models.CharField(max_length=300)
+    
     class Meta:
         db_table = "MachineConditions"
 
 
 class TastingInformations(models.Model):
-    PersonnelId = models.ForeignKey(Personnels,on_delete=models.CASCADE,db_column='PersonnelId')
-    MachineConditionId = models.ForeignKey(MachineConditions,on_delete=models.CASCADE,db_column='MachineConditionId')
-    IntensiveHoursId = models.ForeignKey(IntensiveHours,on_delete=models.CASCADE,db_column='IntensiveHoursId')
-
     TotalEspressoRecipe = models.IntegerField() #Yapılan toplam espresso tarif
     TotalMilkyRecipe = models.IntegerField() #Yapılan toplam sütlü tarif
     DailyCoffeeComsumption = models.IntegerField() #Günlük tüketilen kahve adedi
     DailyShopCoffeeConsumption = models.IntegerField() #Günlük mağazanın kullandığı kahve adeti
     DailyMilkBoxConsumption = models.IntegerField() #Günlük kullanılan süt (kutu)
+    
+    # foreign keys
+    PersonnelId = models.ForeignKey(
+        Personnels,
+        on_delete=models.CASCADE,
+        db_column='PersonnelId')
+    MachineConditionId = models.ForeignKey(
+        MachineConditions,
+        on_delete=models.CASCADE,
+        db_column='MachineConditionId')
+    IntensiveHoursId = models.ForeignKey(
+        IntensiveHours,
+        on_delete=models.CASCADE,
+        db_column='IntensiveHoursId')
+
     class Meta:
         db_table = "TastingInformations"
 
