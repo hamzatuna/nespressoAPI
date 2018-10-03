a77fd0677568ad3f8b0c68fabb2cc1422c7e40ac
tunahamza <tunaham@itu.edu.tr>
2018-10-03 19:47:25 +0000

    some conf

--- nespresso/settings.py
+++ nespresso/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'nespresso.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'postgres',
+        'NAME':'nespressoDB',
         'USER':'postgres',
         'PASSWORD':'1234',
         'HOST':'localhost',
