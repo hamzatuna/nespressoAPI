dcecdeb859fb6a0999003edbb277b0f7adfef4e7
ermissa <ermissa@itu.edu.tr>
2018-10-06 22:22:47 +0300

    Revert "Base.html Eklendi, arayüzde  bir takım değişiklikler yapıldı."
    This reverts commit d2b949df9a99f9cb049f30b6d026326c217a0368.

--- nespressoAPI/views.py
+++ nespressoAPI/views.py
@@ -19,68 +19,9 @@ from .models import *
 import json
 from django.core import serializers
 from datetime import datetime,date,timedelta
-from django.db import connection
 
 def to_json(objects):
     return serializers.serialize('json', objects)
-
-@api_view(['GET','POST'])
-def get_filtered_sales(request):
-    print("DENEME")
-    #print(request.data['personnel_name'])
-    try:
-        cursor = connection.cursor()
-        print(request.POST)
-        #query = '''select "SL"."CustomerName" from "Sales" as "SL" INNER JOIN "Personnels" as "PL" ON ("SL"."PersonnelId" = "PL"."user_id")     INNER JOIN "Machines" as "MC" ON ("SL"."MachineId" = "MC"."id")    INNER JOIN "Locations" as "LC" ON ("SL"."LocationId" = "LC"."id") '''
-        #query = '''select "SL"."CustomerName","SL"."CustomerSurname","SL"."CustomerName","SL"."CustomerPhoneNumber","SL"."CustomerEmail","MC"."Name","LC"."LocationName" from "Sales" as "SL" INNER JOIN "Personnels" as "PL" ON ("SL"."PersonnelId" = "PL"."user_id")     INNER JOIN "Machines" as "MC" ON ("SL"."MachineId" = "MC"."id")    INNER JOIN "Locations" as "LC" ON ("SL"."LocationId" = "LC"."id") '''
-        query = '''select * from "Sales" as "SL" INNER JOIN "Personnels" as "PL" ON ("SL"."PersonnelId" = "PL"."user_id") INNER JOIN "Machines" as "MC" ON ("SL"."MachineId" = "MC"."id")    INNER JOIN "Locations" as "LC" ON ("SL"."LocationId" = "LC"."id") '''
-        if request.method=='POST':
-            print("BURADA")
-
-            if request.data['personnel_name'] and request.data['personnel_name'] is not None:
-                personnel_name = request.data['personnel_name']
-                condition = '''WHERE "PL"."name" = '{0}' '''.format(personnel_name)
-                query = query+condition
-
-            if request.data['personnel_surname'] and request.data['personnel_surname'] is not None:
-                personnel_surname = request.data['personnel_surname']
-                condition = '''WHERE "PL"."surname" = '{0}' '''.format(personnel_surname)
-                query = query+condition
-
-            if request.POST.get("filterObject[startdate]"):
-                startdate = request.data['startdate']
-                condition = '''WHERE "SL"."Date" > '{0}' '''.format(startdate)
-                query = query+condition
-
-            if request.data['enddate'] and request.data['enddate'] is not None:
-                enddate = request.data['enddate']
-                condition = '''WHERE "SL"."Date" < '{0}' '''.format(enddate)
-                query = query+condition
-
-            if request.data['machine_id'] and request.data['machine_id'] is not None:
-                machine_id = request.data['machine_id']
-                condition = '''WHERE "MC"."id" = '{0}' '''.format(machine_id)
-                query = query+condition
-
-            if request.data['is_campaign'] and request.data['is_campaign'] is not None:
-                is_campaign = request.data['is_campaign']
-                condition = '''WHERE "SL"."IsCampaign" = '{0}' '''.format(is_campaign)
-                query = query+condition
-
-            if request.data['location_id'] and request.data['location_id'] is not None:
-                location_id = request.data['location_id']
-                condition = '''WHERE "SL"."LocationId" = '{0}' '''.format(location_id)
-                query = query+condition
-
-            print(query)
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            print(rows)
-            return HttpResponse("",status=200)
-    except Exception as e:
-        print(e)
-        return HttpResponse("",status=500)
-
 
 @api_view(['GET'])
 def get_sales_count(request):
@@ -137,11 +78,3 @@ class TastingInformationsList(generics.ListCreateAPIView):
 class PersonnelsListCreate(generics.ListCreateAPIView):
     serializer_class = PersonnelsSerializer
     queryset = Personnels.objects.all()
-
-class MachinesListCreate(generics.ListCreateAPIView):
-    serializer_class = MachinesSerializer
-    queryset = Machines.objects.all()
-
-class LocationsListCreate(generics.ListCreateAPIView):
-    serializer_class = LocationSerializer
-    queryset = Locations.objects.all()
