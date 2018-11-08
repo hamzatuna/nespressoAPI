b52a84be956b12d3cab4e246de3c94922ada77db
ermissa <ermissa@itu.edu.tr>
2018-11-09 01:40:02 +0300

    Sales filtrelemeleriyle ilgili ufak düzenlemeler

--- nespressoAPI/views.py
+++ nespressoAPI/views.py
@@ -244,60 +244,6 @@ def logout_site(request):
 
 
 
-@api_view(['GET','POST'])
-def get_filtered_sales(request):
-    #print(request.data['personnel_name'])
-    try:
-        cursor = connection.cursor()
-        print(request.POST)
-        #query = '''select "SL"."CustomerName" from "Sales" as "SL" INNER JOIN "Personnels" as "PL" ON ("SL"."personnel_id" = "PL"."user_id")     INNER JOIN "Machines" as "MC" ON ("SL"."MachineId" = "MC"."id")    INNER JOIN "Locations" as "LC" ON ("SL"."location_id" = "LC"."id") '''
-        #query = '''select "SL"."CustomerName","SL"."CustomerSurname","SL"."CustomerName","SL"."CustomerPhoneNumber","SL"."CustomerEmail","MC"."","LC"."name" from "Sales" as "SL" INNER JOIN "Personnels" as "PL" ON ("SL"."personnel_id" = "PL"."user_id")     INNER JOIN "Machines" as "MC" ON ("SL"."MachineId" = "MC"."id")    INNER JOIN "Locations" as "LC" ON ("SL"."location_id" = "LC"."id") '''
-        query = '''select * from "Sales" as "SL" INNER JOIN "Personnels" as "PL" ON ("SL"."personnel_id" = "PL"."user_id") INNER JOIN "Machines" as "MC" ON ("SL"."MachineId" = "MC"."id")    INNER JOIN "Locations" as "LC" ON ("SL"."location_id" = "LC"."id") '''
-        if request.method=='POST':
-            if request.data['personnel_name'] and request.data['personnel_name'] is not None:
-                personnel_name = request.data['personnel_name']
-                condition = '''WHERE "PL"."name" = '{0}' '''.format(personnel_name)
-                query = query+condition
-
-            if request.data['personnel_surname'] and request.data['personnel_surname'] is not None:
-                personnel_surname = request.data['personnel_surname']
-                condition = '''WHERE "PL"."surname" = '{0}' '''.format(personnel_surname)
-                query = query+condition
-
-            if request.POST.get("filterObject[startdate]"):
-                startdate = request.data['startdate']
-                condition = '''WHERE "SL"."Date" > '{0}' '''.format(startdate)
-                query = query+condition
-
-            if request.data['enddate'] and request.data['enddate'] is not None:
-                enddate = request.data['enddate']
-                condition = '''WHERE "SL"."Date" < '{0}' '''.format(enddate)
-                query = query+condition
-
-            if request.data['machine_id'] and request.data['machine_id'] is not None:
-                machine_id = request.data['machine_id']
-                condition = '''WHERE "MC"."id" = '{0}' '''.format(machine_id)
-                query = query+condition
-
-            if request.data['is_campaign'] and request.data['is_campaign'] is not None:
-                is_campaign = request.data['is_campaign']
-                condition = '''WHERE "SL"."IsCampaign" = '{0}' '''.format(is_campaign)
-                query = query+condition
-
-            if request.data['location_id'] and request.data['location_id'] is not None:
-                location_id = request.data['location_id']
-                condition = '''WHERE "SL"."location_id" = '{0}' '''.format(location_id)
-                query = query+condition
-
-            print(query)
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            print(rows)
-            return HttpResponse("",status=200)
-    except Exception as e:
-        print(e)
-        return HttpResponse("",status=500)
-
 
 @api_view(['GET'])
 @manager_required
